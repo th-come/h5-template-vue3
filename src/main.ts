8477e24e19faea5501abadca68c8182ac4b9be2e
tanghao <tanghao@youloft.com>
2021-03-24 09:55:39 +0800

    样式调整

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import App from './App.vue';
 import router from './router';
 import store from './store';
 import 'lib-flexible';
-import '@/assets/common/common.scss';
+import '@/assets/css/common.scss';
 
 createApp(App)
 	.use(store)
